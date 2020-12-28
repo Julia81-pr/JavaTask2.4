9fef963a27ed99111809f702baf0271832ececee
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-28 20:36:39 +0300

    Add files via upload

---
+++ StatisticService.java
@@ -0,0 +1,20 @@
+public class StatisticService {
+    public long calculateSum(long[] purchases) {
+        long sum = 0;
+        for (long purchase : purchases) {
+            // аналог sum = sum + purchase;
+            sum += purchase;
+        }
+        return sum;
+    }
+
+    public long findMax(long[] purchases) {
+        long currentMax = purchases[0];
+        for (long purchase : purchases) {
+            if (currentMax < purchase) {
+                currentMax = purchase;
+            }
+        }
+        return currentMax;
+    }
+}
